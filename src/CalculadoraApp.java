208f8c56526c6a478e7a177b3f3fba28b9ba002a
Anthony140823 <anthonyreyes140803@gmail.com>
2026-08-17 17:11:45 -0500

    Implementación del manejo de excepciones para cuano se ingresa un valor no numérico

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -19,52 +19,58 @@ public class CalculadoraApp {
                     5. Salir
                     """);
 
-            // Pedimos al usuario que ingrese una opción
-            System.out.print("Ingrese una opcion: ");
-            int opcion = Integer.parseInt(sc.nextLine());
+            // Manejamos la exepción si se ingresa una opción no numérica
+            try {
+                // Pedimos al usuario que ingrese una opción
+                System.out.print("Ingrese una opcion: ");
+                int opcion = Integer.parseInt(sc.nextLine());
 
-            // Revisamos que la opción esté dentro de las opciones permitdas
-            if (opcion >= 1 && opcion <= 4) {
+                // Revisamos que la opción esté dentro de las opciones permitdas
+                if (opcion >= 1 && opcion <= 4) {
 
-                // Pedimos al usuario que ingrese el operando 1
-                System.out.print("Proporciona valor de operando1: ");
-                int operando1 = Integer.parseInt(sc.nextLine());
+                    // Pedimos al usuario que ingrese el operando 1
+                    System.out.print("Proporciona valor de operando1: ");
+                    int operando1 = Integer.parseInt(sc.nextLine());
 
-                // Pedimos al usuario que ingrese el operando 2
-                System.out.print("Proporciona valor de operando2: ");
-                int operando2 = Integer.parseInt(sc.nextLine());
+                    // Pedimos al usuario que ingrese el operando 2
+                    System.out.print("Proporciona valor de operando2: ");
+                    int operando2 = Integer.parseInt(sc.nextLine());
 
-                // Incializamos la variable donde se guardará el resultado
-                int resultado;
+                    // Incializamos la variable donde se guardará el resultado
+                    int resultado;
 
-                // Realizamos la operación según la opción elegida
-                switch (opcion) {
-                    case 1 -> { // Suma
-                        resultado = operando1 + operando2;
-                        System.out.println("Resultado Suma: " + resultado);
+                    // Realizamos la operación según la opción elegida
+                    switch (opcion) {
+                        case 1 -> { // Suma
+                            resultado = operando1 + operando2;
+                            System.out.println("Resultado Suma: " + resultado);
+                        }
+                        case 2 -> { // Resta
+                            resultado = operando1 - operando2;
+                            System.out.println("Resultado Resta: " + resultado);
+                        }
+                        case 3 -> { // Multiplicación
+                            resultado = operando1 * operando2;
+                            System.out.println("Resultado Multiplicacion: " + resultado);
+                        }
+                        case 4 -> { // División
+                            resultado = operando1 / operando2;
+                            System.out.println("Resultado Division: " + resultado);
+                        }
+                        default -> System.out.println("Opción erronea!: " + opcion);
                     }
-                    case 2 -> { // Resta
-                        resultado = operando1 - operando2;
-                        System.out.println("Resultado Resta: " + resultado);
-                    }
-                    case 3 -> { // Multiplicación
-                        resultado = operando1 * operando2;
-                        System.out.println("Resultado Multiplicacion: " + resultado);
-                    }
-                    case 4 -> { // División
-                        resultado = operando1 / operando2;
-                        System.out.println("Resultado Division: " + resultado);
-                    }
-                    default -> System.out.println("Opción erronea!: " + opcion);
+                } else if (opcion == 5) { // Salir
+                    System.out.println("Hasta pronto...");
+                    break;
+                } else {
+                    System.out.println("Opción erronea!: " + opcion);
                 }
-            } else if (opcion == 5) { // Salir
-                System.out.println("Hasta pronto...");
-                break;
-            } else {
-                System.out.println("Opción erronea!: " + opcion);
+                // Imprimimos un salto antes de repetir el ciclo par que no se vea todo junto
+                System.out.println();
+            } // fin try
+            catch (Exception e) {
+                System.out.println("Ocurrio un error: " + e.getMessage());
             }
-            // Imprimimos un salto antes de repetir el ciclo par que no se vea todo junto
-            System.out.println();
         } // fin de while
     }
 }
